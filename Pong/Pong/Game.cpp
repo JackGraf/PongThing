f25c7b655415f3cc96a6539038fe2bedcd715dd1
unknown <mrjohngraf@gmail.com>
2021-05-25 21:00:01 -0500

    First commit

---
+++ Pong/Pong/Game.cpp
@@ -0,0 +1,52 @@
+#include "Game.h"
+
+Game::Game()
+{
+	isRunning = true;
+	window = nullptr;
+}
+
+int Game::initialize()
+{
+	SDL_Log("Iniitializing SDL...");
+	int result = SDL_Init(SDL_INIT_EVERYTHING);
+	if (result != 0)
+	{
+		SDL_Log("Failed to initialize SDL. Error: %s", SDL_GetError());
+	}
+
+	window = SDL_CreateWindow("Pong Thing", 100, 100, 1024, 768, 0);
+
+	if (!window)
+	{
+		SDL_Log("Failed to create window: %s", SDL_GetError());
+	}
+	return result;
+}
+
+void Game::runLoop()
+{
+	while (isRunning)
+	{
+		processInput();
+		update();
+		generateOutput();
+	}
+}
+
+
+void Game::shutdown(){
+
+}
+
+void Game::processInput()
+{
+}
+
+void Game::update()
+{
+}
+
+void Game::generateOutput()
+{
+}
---
+++ Pong/Pong/Game.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SDL.h>
+
+class Game
+{
+public:
+	Game();
+	int initialize();
+	void runLoop();
+	void shutdown();
+
+private:
+	void processInput();
+	void update();
+	void generateOutput();
+
+	// Window created by SDL
+	SDL_Window* window;
+
+	bool isRunning;
+};
